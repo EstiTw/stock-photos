99dcd7edafad287fa1ff9b1da52fdc33bc569d16
EstiTw <estitwitowork@gmail.com>
2022-10-31 16:47:00 +0200

    search by query functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 
-const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
-console.log(`?client_id=${process.env.REACT_APP_ACCESS_KEY}`);
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("doll");
+
+  const fetchByQuery = async () => {
+    console.log("fetchByQuery");
+    setLoading(true);
+    try {
+      const response = await fetch(`${searchUrl}?query=${query}&${clientID}`);
+      const data = await response.json();
+      setImages(data.results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const findImages = (e) => {
+    e.preventDefault();
+    fetchByQuery();
+  };
+
   const fetchImages = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`${mainUrl}`);
+      const response = await fetch(`${mainUrl}?${clientID}`);
       const data = await response.json();
       setImages(data);
       console.log(data);
@@ -31,9 +51,14 @@ function App() {
   return (
     <main>
       <section className="search">
-        <form action="" className="search-form">
-          <input type="text" className="form-input" />
-          <button className="submit_btn">
+        <form action="" className="search-form" onSubmit={(e) => findImages(e)}>
+          <input
+            type="text"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="submit_btn" onClick={(e) => findImages(e)}>
             <FaSearch />
           </button>
         </form>
